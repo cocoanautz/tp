c307d38b4c56b4ee0793c0c8b2b8fee30a2f570f
Isaac <isaacong1@hotmail.com>
2024-03-12 17:26:42 +0800

    Add remark command
    Remark command allows the user to add remarks to persons

---
+++ src/main/java/seedu/address/logic/parser/exceptions/RemarkCommandParser.java
@@ -0,0 +1,47 @@
+package seedu.address.logic.parser.exceptions;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.RemarkCommand;
+import seedu.address.logic.parser.ArgumentMultimap;
+import seedu.address.logic.parser.ArgumentTokenizer;
+import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.ParserUtil;
+import seedu.address.model.person.Remark;
+
+/**
+ * Parses input arguments and creates a new RemarkCommand Object.
+ */
+public class RemarkCommandParser implements Parser {
+
+    /**
+     * Parses {@code userInput} into a command and returns it.
+     *
+     * @param args
+     * @throws ParseException if {@code userInput} does not conform the expected format
+     */
+    @Override
+    public Command parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args,
+                PREFIX_REMARK);
+
+        Index index;
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (IllegalValueException ive) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    RemarkCommand.MESSAGE_USAGE), ive);
+        }
+
+        Remark remark = new Remark(argMultimap.getValue(PREFIX_REMARK).orElse(""));
+
+        return new RemarkCommand(index, remark);
+
+    }
+}
--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -10,7 +10,7 @@ import javafx.scene.layout.Region;
 import seedu.address.model.person.Person;
 
 /**
- * An UI component that displays information of a {@code Person}.
+ * A UI component that displays information of a {@code Person}.
  */
 public class PersonCard extends UiPart<Region> {
 
@@ -39,6 +39,8 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private Label email;
     @FXML
+    private Label remark;
+    @FXML
     private FlowPane tags;
 
     /**
@@ -55,5 +57,6 @@ public class PersonCard extends UiPart<Region> {
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+        remark.setText(person.getRemark().value);
     }
 }
